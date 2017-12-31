714ab403694c86232fa646d5518e635acb1a6e3e
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 17:00:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,6 +24,18 @@ class Dog
     DB[:conn].execute("DROP TABLE dogs;")
   end
 
-
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (? , ?);
+        SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")
+    end
+  end
+  
 
 end
